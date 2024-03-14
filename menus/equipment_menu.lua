96feb3c730971900379f5031a285c655f6c141b3
Jake Murray <j1993murray@hotmail.com>
2024-03-14 18:14:23 +0000

    Update options on equipment buying menu

--- menus/equipment_menu.lua
+++ menus/equipment_menu.lua
@@ -1,5 +1,7 @@
 RegisterNetEvent('lumberjack:client:EquipmentMenu', function()
 	local headerMenu = {}
+    local moneyAmount = exports.ox_inventory:Search('count', 'money')
+    local description = nil
 
     headerMenu[#headerMenu + 1] = {
         title = "Cutting axe",
@@ -8,15 +10,17 @@ RegisterNetEvent('lumberjack:client:EquipmentMenu', function()
         args = Config.Lumberyard.EquipmentBuying.TreeChoppingItem,
         icon = 'fa-solid fa-tree',
         iconColor = "yellow",
+        disabled = moneyAmount < Config.Lumberyard.EquipmentBuying.TreeChoppingItem.Price,
     }
 
     headerMenu[#headerMenu + 1] = {
         title = "Table Saw",
-        description = "Purcahse a table saw to help you cut your logs into planks",
+        description = "Purchase a table saw to help you cut your logs into planks",
         serverEvent = 'lumberjack:server:PurchaseEquipment',
         args = Config.Lumberyard.EquipmentBuying.TableSawItem,
         icon = 'fa-solid fa-fan',
         iconColor = "yellow",
+        disabled = moneyAmount < Config.Lumberyard.EquipmentBuying.TableSawItem.Price,
     }
 
     lib.registerContext({
